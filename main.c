a48d0435514215e0f62c44a021778fd5ebc7401a
Shawn Brown <shiningsilence13@gmail.com>
2020-10-30 00:22:11 -0500

    main.c modified

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "token.h"
+#include "scanner.h"
+
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+FILE *yyin;
+
+
+int main(int argc, char* argv[]){
+
+    int ntoken, vtoken;
+    if(argc < 2){
+        printf("Entering data: \n");
+        ntoken = yylex();
+        while(ntoken){
+            printf("Token: %s, Data: %s, Line: %d\n",tokenNames[ntoken], yytext, yylineno);
+            ntoken = yylex();
+        }
+
+    }
+    else if(argc == 2){
+
+        yyin = fopen(argv[1], "r");
+        if(yyin == NULL)
+        {
+            perror(argv[1]);
+            printf("ERROR: file does not exist.");
+            return 0;
+        }
+        else {
+            ntoken = yylex();
+            while (ntoken) {
+                printf("Token: %s, Data: %s, Line: %d\n", tokenNames[ntoken], yytext, yylineno);
+                ntoken = yylex();
+            }
+        }
+    }
+    return 0;
+}
--- scanner.c
+++ scanner.c
@@ -13,6 +13,7 @@ int main(int argc, char* argv[]){
 	
 	int ntoken, vtoken;
 	if(argc < 2){
+	    printf("Entering data: \n");
 		ntoken = yylex();
 		while(ntoken){
 			printf("Token: %s, Data: %s, Line: %d\n",tokenNames[ntoken], yytext, yylineno);
@@ -21,12 +22,21 @@ int main(int argc, char* argv[]){
 
 	}
 	else if(argc == 2){
+
 		yyin = fopen(argv[1], "r");
-		ntoken = yylex();
-		while(ntoken){
-			printf("Token: %s, Data: %s, Line: %d\n", tokenNames[ntoken], yytext, yylineno);
-			ntoken = yylex();
-		}
+        if(yyin == NULL)
+        {
+            perror(argv[1]);
+            printf("ERROR: file does not exist.");
+            return 0;
+        }
+        else {
+            ntoken = yylex();
+            while (ntoken) {
+                printf("Token: %s, Data: %s, Line: %d\n", tokenNames[ntoken], yytext, yylineno);
+                ntoken = yylex();
+            }
+        }
 	}
 	return 0;
 }
